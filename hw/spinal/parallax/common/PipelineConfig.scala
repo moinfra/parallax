b8cd4e51912c62a693752bcf815ac663faaae400
pluveto <zijing.zhang@proton.me>
2025-05-20 22:45:05 +0800

    Update PipelineConfig with fetch width and derived calculations

--- hw/spinal/parallax/common/PipelineConfig.scala
+++ hw/spinal/parallax/common/PipelineConfig.scala
@@ -5,9 +5,10 @@ import spinal.lib._
 
 case class PipelineConfig() {
   val xlen: Int = 32
-  // GPR/FPR data width
+  val fetchWidth: Int = 2 // Number of instructions to fetch in parallel (e.g., 2 for a 2-wide superscalar)
+  // GPR/FPR data width (also individual instruction width)
   val dataWidth: BitCount = xlen bits
-  // Program Counter width
+  // Program Counter width & general address width for memory access
   val pcWidth: BitCount = xlen bits
   // Architectural GPR/FPR index (e.g. 2^5 = 32 regs)
   val archGprIdxWidth: BitCount = 5 bits
@@ -21,6 +22,11 @@ case class PipelineConfig() {
   val robDepth: Int = 64
   // For microarchitecture-specific fields
   val uopUniqueIdWidth: BitCount = 16 bits
+
+  // Derived widths
+  def bytesPerInstruction: Int = dataWidth.value / 8
+  def fetchGroupBytes: Int = fetchWidth * bytesPerInstruction
+
   def physGprIdxWidth: BitCount = log2Up(numPhysGprs) bits
   def physFprIdxWidth: BitCount = log2Up(numPhysFprs) bits // If different from GPRs
   def robIdxWidth: BitCount = log2Up(robDepth) bits
