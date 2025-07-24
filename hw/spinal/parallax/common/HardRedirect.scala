b8d5c6b878dc1b0ab089c73795c52a9bf980ab74
pluveto <zijing.zhang@proton.me>
2025-07-24 16:40:39 +0800

    Add SoftRedirectService trait
    Added a new trait `SoftRedirectService` that extends `Service` with `LockedImpl`. This trait includes a method `doSoftRedirect()` which returns a `Bool`. This new trait provides functionality for a soft redirect operation, in addition to the existing hard redirect functionality provided by `HardRedirectService`.

--- hw/spinal/parallax/common/HardRedirect.scala
+++ hw/spinal/parallax/common/HardRedirect.scala
@@ -6,3 +6,7 @@ import parallax.utilities.LockedImpl
 trait HardRedirectService extends Service with LockedImpl {
     def doHardRedirect(): Bool
 }
+
+trait SoftRedirectService extends Service with LockedImpl {
+    def doSoftRedirect(): Bool
+}
